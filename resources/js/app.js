4cc660cb3898a1081f04474cea6dd75b9d949128
jithinlal <jithtitan007@gmail.com>
2019-04-18 22:39:38 +0530

    full initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,41 @@
+require('./bootstrap');
+window.Vue = require('vue');
+
+
+Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
+Vue.component('chat-form', require('./components/ChatForm.vue').default);
+
+const app = new Vue({
+	el: '#app',
+
+	data: {
+		messages: []
+	},
+
+	created() {
+		this.fetchMessages();
+		Echo.private('chat')
+			.listen('MessageSent', (e) => {
+				this.messages.push({
+					message: e.message.message,
+					user: e.user
+				});
+			});
+	},
+
+	methods: {
+		fetchMessages() {
+			axios.get('/messages').then(res => {
+				this.messages = res.data;
+			});
+		},
+
+		addMessage(message) {
+			this.messages.push(message);
+
+			axios.post('/messages', message).then(res => {
+				console.log(res.data);
+			});
+		}
+	}
+})
